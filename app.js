b200c08571d12e855be509abc5c96a769ccd1b27
AmrElsherbiny88 <amoryelsherbiny@gmail.com>
2023-06-15 13:45:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// loading page start
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+//   loading page end
+
+
+
+
+
+
+
+
+
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
